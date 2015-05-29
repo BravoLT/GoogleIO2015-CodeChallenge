4a64ef8cd1985ff0eb6e4d57bfc9fbebfc91b30a
Brett VanderVeen <veenified@gmail.com>
2015-05-29 12:54:06 -0400

    Update run.sh for Challenge2 update

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 echo "\n>>>>> Challenge1 Solution: <<<<<";
 ruby Challenge1/FizzBuzz.rb 1 100;
 echo "\n>>>>> Challenge2 Solution: <<<<<";
-ruby Challenge2/NameRank.rb Challenge2/test_input.txt;
+ruby Challenge2/NameRank.rb Challenge2/test_input.txt 10;
